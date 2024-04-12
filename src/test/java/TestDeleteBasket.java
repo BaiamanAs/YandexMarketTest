e43abc377194f2c34514d9f22d68df41d9d2f565
BaiamanAs <166730471+BaiamanAs@users.noreply.github.com>
2024-04-12 10:30:50 +0500

    Update TestDeleteBasket.java

--- src/test/java/TestDeleteBasket.java
+++ src/test/java/TestDeleteBasket.java
@@ -8,18 +8,15 @@ public class TestDeleteBasket extends BaseUITest {
     @Test
     @DisplayName("Тест-кейс №2")
     public void DeleteFromBasket() throws InterruptedException {
-        Thread.sleep(8000);
         welcome.ExBTN.click();
         welcome.GiveAccess.click();
         welcome.AccessAdress.click();
         mainPage.CatalogBTN.click();
         catalog.ScrollToName.scrollTo();
-        Thread.sleep(2000);
         catalog.ScrollToName.click();
         books.FirstBook.scrollTo();
         books.FirstBook.click();
         basket.BasketBTN.click();
-        Thread.sleep(2000);
         basket.DeleteBTN.click();
         Assertions.assertEquals(basket.CheckEmptyBasket.getText(),"Корзина пустая");
 
